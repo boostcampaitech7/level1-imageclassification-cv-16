3f4d397efc11101e0fb9fd8562cae4b2a5cb35c7
Ki_Hoon_Kwak <kihoon090@gmail.com>
2024-09-11 01:24:41 +0900

    Feat/ 원핫인코딩 Fixes #6

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -14,7 +14,8 @@ class CustomDataset(Dataset):
         root_dir: str,
         data_df: pd.DataFrame, 
         transform: Callable = None, 
-        is_inference: bool = False  
+        is_inference: bool = False,
+        one_hot: bool = False  
         ):
         # CustomDataset 초기화
         # 매개변수 
@@ -28,6 +29,11 @@ class CustomDataset(Dataset):
         self.transform = transform
         self.is_inference = is_inference
         self.image_paths = data_df['image_path'].tolist()
+        self.one_hot = one_hot
+        if self.one_hot:
+            # class 개수 크기의 one_hot template 생성 
+            # 비효율적일 수 있음 / target이 정수인 경우에만 동작함
+            self.one_hot_template = [0 for i in range(len(data_df['target'].unique()))]
         
         if not self.is_inference:
             self.targets = data_df['target'].tolist()
@@ -41,22 +47,43 @@ class CustomDataset(Dataset):
         # 반환 값 : is_inference=False => 이미지, 레이블, is_inference=True => 이미지
         img_path = os.path.join(self.root_dir, self.image_paths[idx]) # 완전한 이미지 path로 만들기
         img = cv2.imread(img_path, cv2.IMREAD_COLOR) # BGR 컬러 포맷인 numpy 배열로 읽어옴
-        img = cv2.cvtcolor(img, cv2.COLOR_BGR2RGB) # BGR 포맷을 RGB 포맷으로 변환
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # BGR 포맷을 RGB 포맷으로 변환
         img = self.transform(img) # 이미지 변환 수행
         
         if self.is_inference: # 추론 시
             return img # 이미지만 반환
         else: # 학습 시
-            target = self.targets[idx] # 해당 이미지의 레이블 
+            target = self.targets[idx] # 해당 이미지의 레이블
+            if self.one_hot: # One_hot으로 반환하고자 하는 경우
+                temp = self.one_hot_template.copy() # template을 가져옴
+                temp[target] = 1 # 레이블에 해당하는 위치에 1 나머지는 0
+                target = temp # 반환을 위해 target에 할당
             return img, target # 이미지와 레이블 반환
                
 # # 테스트용
 # def test(csv_file=None, root_dir=None, transform=None):
-#     CD = CustomDataset(csv_file, root_dir, transform)
+#     df = pd.read_csv(csv_file)
+#     train = CustomDataset(root_dir, df, transform)
+#     test = CustomDataset(root_dir, df, transform, True)
+    
+#     print("testing for train & test")
+#     img, target = train.__getitem__(0)
+#     print(f"train idx 0 : {img.shape}, {target}")
+#     img = test.__getitem__(0)
+#     print(f"train idx 0 : {img.shape}")
+    
+#     one_hot_train = CustomDataset(root_dir, df, transform, one_hot=True)
+
+#     print("testing for train one_hot")
+#     img, target = one_hot_train.__getitem__(0)
+#     print(f"train idx 0 : {img.shape}, {target}")
+#     img, target = one_hot_train.__getitem__(1)
+#     print(f"train idx 1 : {img.shape}, {target}")
+    
     
 # transform = transforms.Compose([
 #     transforms.ToTensor(),
 #     # transforms.Normalize((0.485,0.456,0.406), (0.229, 0.224, 0.225))
 # ])
 
-# test("", "./data", transform)
+# test("data/test_sample.csv", "./data", transform)
