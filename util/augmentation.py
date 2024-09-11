fdd90202259faf4ae2141fe43b35e88c27bc52ab
Ja2Hw <mevest71@naver.com>
2024-09-11 18:22:42 +0900

    Transforms Augmentation v2 - type hinting

--- util/augmentation.py
+++ util/augmentation.py
@@ -3,11 +3,12 @@ import random
 import torchvision.transforms as T
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from typing import Union, Tuple
 
 
 #기본 트랜스폼 클래스
 class BasicTransforms:
-    def __init__(self, augment=False):
+    def __init__(self, augment=False) -> None:
         self.augment = augment
 
         # 기본 트랜스폼 (증강 없음)
@@ -25,7 +26,7 @@ class BasicTransforms:
             T.ToTensor(), #이미지를 텐서로 변환
         ])
 
-    def __call__(self, image): #이미지에 트랜스폼 적용함
+    def __call__(self, image) -> torch.tensor: #이미지에 트랜스폼 적용함
         if self.augment:
             return self.augment_transform(image)
         return self.base_transform(image)
@@ -33,7 +34,7 @@ class BasicTransforms:
 
 # Albumentation 기반 트랜스폼
 class AlbumentationsTransforms:
-    def __init__(self, augment=False): #True면 증강을 포함한 트랜스폼 적용, False면 기본 트랜스폼 적용
+    def __init__(self, augment=False) -> None: #True면 증강을 포함한 트랜스폼 적용, False면 기본 트랜스폼 적용
         self.augment = augment
 
         # 증강 없는 기본 트랜스폼
@@ -52,7 +53,7 @@ class AlbumentationsTransforms:
             ToTensorV2(), #이미지를 텐서로 변환
         ])
 
-    def __call__(self, image): #이미지에 트랜스폼 적용
+    def __call__(self, image) -> torch.Tensor: #이미지에 트랜스폼 적용
         if self.augment:
             augmented = self.augment_transform(image=image)
             return augmented['image']
@@ -63,10 +64,10 @@ class AlbumentationsTransforms:
 
 #컷믹스 트랜스폼
 class CutMixTransforms:
-    def __init__(self, alpha=1.0): #알파: CutMix에서 사용할 베타 분포 파라미터
+    def __init__(self, alpha=1.0) -> None: #알파: CutMix에서 사용할 베타 분포 파라미터
         self.alpha = alpha
 
-    def __call__(self, image1, image2, label1, label2): #두 개 이미지를 컷믹스
+    def __call__(self, image1, image2, label1, label2) -> Tuple[torch.Tensor, torch.Tensor]: #두 개 이미지를 컷믹스
         lam = random.beta(self.alpha, self.alpha) #베타 분포로 람다 값 생성
         bbx1, bby1, bbx2, bby2 = self.rand_bbox(image1.size(), lam) #랜덤 바운딩 박스 생성
         image1[:, bbx1:bbx2, bby1:bby2] = image2[:, bbx1:bbx2, bby1:bby2] #바운딩 박스 내 영역 교체함
@@ -75,7 +76,7 @@ class CutMixTransforms:
         return image1, mixed_label
 
     @staticmethod
-    def rand_bbox(size, lam): #람다 값을 기준으로 랜덤한 바운딩 박스 생성
+    def rand_bbox(size, lam) -> Tuple[int, int, int, int]: #람다 값을 기준으로 랜덤한 바운딩 박스 생성
         W = size[2]
         H = size[1]
         cut_rat = torch.sqrt(1. - lam) #자를 비율 계산
@@ -95,10 +96,10 @@ class CutMixTransforms:
 
 # 믹스업 트랜스폼
 class MixUpTransforms:
-    def __init__(self, alpha=1.0): #알파: 믹스업에서 사용할 베타 분포의 파라미터
+    def __init__(self, alpha=1.0) -> None: #알파: 믹스업에서 사용할 베타 분포의 파라미터
         self.alpha = alpha
 
-    def __call__(self, image1, image2, label1, label2): #두 개 이미지를 믹스업
+    def __call__(self, image1, image2, label1, label2) -> Tuple[torch.Tensor, torch.Tensor]: #두 개 이미지를 믹스업
         lam = random.beta(self.alpha, self.alpha)  # 베타 분포로 람다 값 생성
         mixed_image = lam * image1 + (1 - lam) * image2  # 이미지를 섞음
         mixed_label = lam * label1 + (1 - lam) * label2  # 레이블을 섞음
@@ -108,11 +109,11 @@ class MixUpTransforms:
 # 트랜스폼 데이터 증강 라이브러리와 기법을 선택하는 클래스
 class TransformSelector: #사용자가 지정한 transform_type에 따라 서로 다른 변환 객체를 반환하는 구조
 
-    def __init__(self, transform_type: str):
+    def __init__(self, transform_type: str) -> None:
         self.transform_type = transform_type
         #인자 확인해서 변환 라이브러리를 선택함
 
-    def get_transform(self, augment=False, alpha=1.0):
+    def get_transform(self, augment=False, alpha=1.0) -> Union[BasicTransforms, AlbumentationsTransforms, CutMixTransforms, MixUpTransforms]:
         """
         augment: 데이터 증강 여부
         alpha: MixUp이나 CutMix의 파라미터
