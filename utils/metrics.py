42a3a819b9cac420459b231d120255614f64521d
yuhyang <ghddbgid2596@gmail.com>
2024-09-11 18:51:51 +0900

    Feat/ confusin matrix 새로 고침

--- utils/metrics.py
+++ utils/metrics.py
@@ -21,3 +21,18 @@ def calculate_metrics(
         "f1": f1,
         "confusion_matrix": cm
     }
+
+def calculate_accuracy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    return accuracy_score(y_true, y_pred)
+
+def calculate_precision(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    return precision_score(y_true, y_pred, average='macro')
+
+def calculate_recall(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    return recall_score(y_true, y_pred, average='macro')
+
+def calculate_f1(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    return f1_score(y_true, y_pred, average='macro')
+
+def calculate_confusion_matrix(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    return confusion_matrix(y_true, y_pred)
