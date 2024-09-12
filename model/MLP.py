9171acdfc28f256862bdc7b91382370054ac40d4
Ja2Hw <mevest71@naver.com>
2024-09-12 22:57:14 +0900

    Add model v1

--- model/MLP.py
+++ model/MLP.py
@@ -1,1 +1,15 @@
 import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    def __init__(self, input_size: int, hidden_size: int, num_classes: int):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.flatten(x, 1)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
