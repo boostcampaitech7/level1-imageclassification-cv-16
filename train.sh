6038f079b86312661befe83b966b82662a2d656d
kkh090 <kihoon090@gmail.com>
2024-09-23 17:13:37 +0900

    style/ train.sh 설명 추가

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 # 실행은 Terminal에 sh exp.sh
 # 학습 재개를 원한다면 인자로 --resume True와 --weights_path [학습재개하고자하는 가중치 파일]을 넘겨주세요.
+# timm- 관련 모델을 사용하려고 한다면 timm-resnet50, timm-resnet18 처럼 작성해주면 됨
 python train.py \
     --mode train \
     --device cuda \
